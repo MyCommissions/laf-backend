5d2a93372c32a914745dc289b53b9a506921423f
rairucrausus <89209183+TuftyRairu@users.noreply.github.com>
2025-10-15 13:04:22 +0800

    few additional on integration (#8)

--- models/matchedItem.js
+++ models/matchedItem.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const matchedItemSchema = new mongoose.Schema(
   {
@@ -19,6 +19,30 @@ const matchedItemSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       default: null,
+    },
+
+    // 🟢 Added claim information (does NOT replace existing fields)
+    claimInfo: {
+      imageUuid: {
+        type: String,
+        default: null,
+      },
+      contactNumber: {
+        type: String,
+        default: null,
+      },
+      firstName: {
+        type: String,
+        default: null,
+      },
+      lastName: {
+        type: String,
+        default: null,
+      },
+      timeOfClaim: {
+        type: Date,
+        default: null,
+      },
     },
   },
   { timestamps: true, versionKey: false }
