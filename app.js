dafd3f39a022e4efaa631320520b40e1575a1b3e
TuftyRairu <craususrhyle@gmail.com>
2025-11-19 02:25:30 +0800

    deployment

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
-const VERSION = process.env.VERSION || v1;
+const VERSION = process.env.VERSION || "v1";
 
 dbConnect();
 
