830da4120a7cb5166de909d00d308bc2fb9af32d
TuftyRairu <craususrhyle@gmail.com>
2025-11-20 04:01:25 +0800

    deployment

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -117,7 +117,7 @@ const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "none",
+      sameSite: "lax",
       path: "/",
       maxAge: 60 * 60 * 1000, // 1 hour
     });
@@ -195,7 +195,7 @@ const logout = async (req, res) => {
     res.clearCookie("token", {
       httpOnly: true,
       secure: true, // set to true if HTTPS in production
-      sameSite: "none",
+      sameSite: "lax",
       path: "/",
     });
 
