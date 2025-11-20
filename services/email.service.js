adae554d6594a07647e8583002f81f1ac18c2bb1
TuftyRairu <craususrhyle@gmail.com>
2025-11-20 18:21:44 +0800

    deployment

--- services/email.service.js
+++ services/email.service.js
@@ -1,21 +1,28 @@
-const MailerSend = require("@mailersend/mailersend");
+const { MailerSend, EmailParams, Sender, Recipient } = require("mailersend");
 require("dotenv").config();
 
-const mailersend = new MailerSend({ apiKey: process.env.MAILERSEND_API_KEY });
+const mailersend = new MailerSend({
+  apiKey: process.env.MAILERSEND_API_KEY,
+});
 
-const sendEmail = async (to, subject, text, html) => {
+const sendEmail = async (to, subject, text = "", html = "") => {
   try {
-    const response = await mailersend.email.send({
-      from: process.env.EMAIL_FROM,
-      to: [to],
-      subject,
-      text,
-      html,
-    });
+    const sender = new Sender(process.env.EMAIL_FROM); // can also include name
+    const recipient = new Recipient(to);
 
+    const emailParams = new EmailParams()
+      .setFrom(sender)
+      .setTo([recipient])
+      .setSubject(subject)
+      .setText(text)
+      .setHtml(html);
+
+    const response = await mailersend.email.send(emailParams);
     console.log("📧 Email sent successfully:", response);
+    return response;
   } catch (err) {
     console.error("❌ Email send failed:", err.message);
+    throw err;
   }
 };
 
