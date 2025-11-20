4a3fe3d6419793559f24b04645d756f83f7b44ab
TuftyRairu <craususrhyle@gmail.com>
2025-11-20 15:49:21 +0800

    deployment

--- services/email.service.js
+++ services/email.service.js
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (to, subject, text) => {
   try {
     const { data, error } = await resend.emails.send({
-      from: "Lost & Found <no-reply@claime.site>",
+      from: "Lost & Found <no-reply@resend.com>",
       to,
       subject,
       text,
